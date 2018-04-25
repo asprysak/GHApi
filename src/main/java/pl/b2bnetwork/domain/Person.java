527a9deb5897bc969fb8533f4875c96b8bd0faa2
Anna Sprysak <a.sprysak.pl@gmail.com>
2018-04-26 01:47:45 +0200

    restore Person class

--- src/main/java/pl/b2bnetwork/domain/Person.java
+++ src/main/java/pl/b2bnetwork/domain/Person.java
@@ -1,21 +1,12 @@
 package pl.b2bnetwork.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-import lombok.ToString;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Data
 @Builder
 public class Person {
 
-    private Long id;
     private String login;
-    @JsonProperty("id")
-    private long personId;
+    private long id;
 }
