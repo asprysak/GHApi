edb67998a7b0f8ee9d54018804ae7378cb1bda01
Anna Sprysak <a.sprysak.pl@gmail.com>
2018-04-26 03:24:05 +0200

    change RepoRepository and UserRepository to extend JpaRepository

--- src/main/java/pl/b2bnetwork/repository/UserRepository.java
+++ src/main/java/pl/b2bnetwork/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package pl.b2bnetwork.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.b2bnetwork.domain.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByLogin(String login);
+    boolean existsByLogin(String login);
 }
