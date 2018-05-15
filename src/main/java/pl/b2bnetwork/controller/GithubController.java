dbe51e37920ab7ef3dcecd99b51bdaa24c407b77
AgataDziubala <agata.dziubala@gmail.com>
2018-05-15 02:16:02 +0200

    Add githubController to templates

--- src/main/java/pl/b2bnetwork/controller/GithubController.java
+++ src/main/java/pl/b2bnetwork/controller/GithubController.java
@@ -2,36 +2,59 @@ package pl.b2bnetwork.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import pl.b2bnetwork.domain.Gist;
+import pl.b2bnetwork.domain.User;
+import pl.b2bnetwork.domain.UserMaker;
 import pl.b2bnetwork.dto.RepoDto;
+import pl.b2bnetwork.dto.UserDto;
+import pl.b2bnetwork.dto.UserDtoToUserConverter;
 import pl.b2bnetwork.service.GithubService;
 
 import java.util.List;
 
 @RequestMapping("/github")
-@RestController
+@Controller
 public class GithubController {
 
     @Autowired
     private GithubService githubService;
 
-    @RequestMapping("/howManyFollowers")
-    public String howManyFollowers(String login) {
-        return Integer.toString(githubService.howManyFollowersThePersonHas(login));
+    private UserMaker userMaker = new UserMaker();
+
+    private UserDtoToUserConverter userConverter = new UserDtoToUserConverter();
+
+    @RequestMapping(value = "/howManyFollowers")
+    public String howManyFollowers(Model model, @ModelAttribute User user) {
+        UserDto userDto = userMaker.makeUser(user.getLogin());
+        user = userConverter.convert(userDto);
+        String stat = Integer.toString(githubService.howManyFollowersThePersonHas(user.getLogin()));
+        model.addAttribute("stats", "Followers: " + stat);
+        return "howManyFollowers";
     }
 
-    @RequestMapping("/howManyRepos")
-    public String howManyRepos(String login) {
-        return Integer.toString(githubService.howManyReposThePersonHas(login));
+/*    @RequestMapping(value = "/howManyRepos")
+    public String howManyRepos(Model model, @ModelAttribute User user) {
+        UserDto userDto = userMaker.makeUser(user.getLogin());
+        user = userConverter.convert(userDto);
+        String stat = Integer.toString(githubService.howManyReposThePersonHas(user.getLogin()));
+        model.addAttribute("stats", "Repos: " + stat);
+        return "howManyRepos";
     }
 
     @RequestMapping("/howManyDays")
-    public String howManyDays(String login) {
-        return githubService.howManyDaysAgoTheAccountWasCreated(login);
-    }
-
+    public String howManyDays(Model model, @ModelAttribute User user) {
+        UserDto userDto = userMaker.makeUser(user.getLogin());
+        user = userConverter.convert(userDto);
+        String stat = githubService.howManyDaysAgoTheAccountWasCreated(user.getLogin());
+        model.addAttribute("stats", "Days on Github: " + stat);
+        return "howManyDays";
+    }*/
+/*
     @RequestMapping("/averageNoOfFollowersOfFollowers")
     public String averageNoOfFollowersOfFollowers(String login) {
         return Double.toString(githubService.averageNoOfFollowersOfFollowers(login));
@@ -65,5 +88,5 @@ public class GithubController {
     @RequestMapping("/reposWrittenInASpecificLanguage")
     public List<RepoDto> reposWrittenMostlyInASpecificLanguage(String login, String lang) {
         return githubService.reposWrittenMostlyInASpecificLanguage(login, lang);
-    }
+    }*/
 }
